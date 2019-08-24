931be1c2316bb9ea765506876c116ff0630e8269
Siddharth Deore <siddharthdeore@gmail.com>
2019-08-24 14:40:19 +0200

    c

--- utilities.py
+++ utilities.py
@@ -1,5 +1,5 @@
 import os
-# for file handling
+# for file handling converets relative path to absolute path
 def absolute_file_path(rel_path):
     script_dir = os.path.dirname(__file__)
     return os.path.join(script_dir, rel_path)
